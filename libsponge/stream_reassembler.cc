8cc653eb8d0906b157d297f5d63f977f25174668
liuyuepeng <1026987156@qq.com>
2024-12-20 06:22:34 -0500

    stream_reassembler coding

---
+++ libsponge/stream_reassembler.cc
@@ -0,0 +1,34 @@
+#include "stream_reassembler.hh"
+
+#include <cassert>
+
+// Dummy implementation of a stream reassembler.
+
+// For Lab 1, please replace with a real implementation that passes the
+// automated checks run by `make check_lab1`.
+
+// You will need to add private members to the class declaration in `stream_reassembler.hh`
+
+template <typename... Targs>
+void DUMMY_CODE(Targs &&.../* unused */) {}
+
+using namespace std;
+
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _unassemble_strs()
+    , _next_assembled_idx(0)
+    , _unassembled_bytes_num(0)
+    , _eof_idx(-1)
+    , _output(capacity)
+    , _capacity(capacity) {}
+
+//! \details This function accepts a substring (aka a segment) of bytes,
+//! possibly out-of-order, from the logical stream, and assembles any newly
+//! contiguous substrings and writes them into the output stream in order.
+void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
+
+}
+
+size_t StreamReassembler::unassembled_bytes() const {}
+
+bool StreamReassembler::empty() const { }
---
+++ libsponge/stream_reassembler.hh
@@ -0,0 +1,56 @@
+#ifndef SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
+#define SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
+
+#include "byte_stream.hh"
+
+#include <cstdint>
+#include <map>
+#include <string>
+
+//! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
+//! possibly overlapping) into an in-order byte stream.
+class StreamReassembler {
+  private:
+    // Your code here -- add private members as necessary.
+    std::map<size_t, std::string> _unassemble_strs;   // 使用map，用于存储未组装的字节流片段，通过索引排序
+    size_t _next_assembled_idx;                       // 记录下一个要组装的字节索引，确保字节按顺序组装
+    size_t _unassembled_bytes_num;                    // 记录尚未组装的字节数量，确保不超过容量
+    size_t _eof_idx;                                  // eof索引
+
+    ByteStream _output;  //!< The reassembled in-order byte stream  lab0实现的字节流
+    size_t _capacity;    //!< The maximum number of bytes   Ressembler缓冲区最大容量
+
+  public:
+    //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
+    //! \note This capacity limits both the bytes that have been reassembled,
+    //! and those that have not yet been reassembled.
+    StreamReassembler(const size_t capacity);
+
+    //! \brief Receive a substring and write any newly contiguous bytes into the stream.
+    //!
+    //! The StreamReassembler will stay within the memory limits of the `capacity`.
+    //! Bytes that would exceed the capacity are silently discarded.
+    //!
+    //! \param data the substring
+    //! \param index indicates the index (place in sequence) of the first byte in `data`
+    //! \param eof the last byte of `data` will be the last byte in the entire stream
+    void push_substring(const std::string &data, const uint64_t index, const bool eof);
+
+    //! \name Access the reassembled byte stream
+    //!@{
+    const ByteStream &stream_out() const { return _output; }
+    ByteStream &stream_out() { return _output; }
+    //!@}
+
+    //! The number of bytes in the substrings stored but not yet reassembled
+    //!
+    //! \note If the byte at a particular index has been pushed more than once, it
+    //! should only be counted once for the purpose of this function.
+    size_t unassembled_bytes() const;
+
+    //! \brief Is the internal state empty (other than the output stream)?
+    //! \returns `true` if no substrings are waiting to be assembled
+    bool empty() const;
+};
+
+#endif  // SPONGE_LIBSPONGE_STREAM_REASSEMBLER_HH
